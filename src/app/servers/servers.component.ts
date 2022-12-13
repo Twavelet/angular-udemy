b9084704f609f30c31a9babaa6cbfdbe160a38cb
trevorwavelet@gmail.com <trevorwavelet@gmail.com>
2022-12-13 10:36:16 -0500

    saving completed assignment

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,16 +9,21 @@ export class ServersComponent {
   allowNewServer = false;
   serverCreationStatus: string = "No server created yet";
   serverName: string = ''
+  userName: string = ''
 
   constructor(){
     setTimeout(()=> this.allowNewServer = true, 2000)
   }
 
   onCreateServer(){
-    this.serverCreationStatus = 'Server was created.'
+    this.serverCreationStatus = 'Server was created. The server name is ' + this.serverName
   }
 
   onUpdateServerName(event: Event){
     this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+  clearUsername(){
+    this.userName = ''
   }
 }
